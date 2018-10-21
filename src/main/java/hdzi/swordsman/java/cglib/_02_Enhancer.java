9efe8048df7a3063b8b34c392c938257f848160f
Hdzi <taojinhou@qq.com>
2018-10-22 00:52:21 +0800

    2018/10/22/00/39

--- src/main/java/hdzi/swordsman/java/cglib/_02_Enhancer.java
+++ src/main/java/hdzi/swordsman/java/cglib/_02_Enhancer.java
@@ -1,9 +1,8 @@
 package hdzi.swordsman.java.cglib;
 
-import net.sf.cglib.proxy.Callback;
-import net.sf.cglib.proxy.Enhancer;
-import net.sf.cglib.proxy.FixedValue;
-import net.sf.cglib.proxy.NoOp;
+import net.sf.cglib.proxy.*;
+
+import java.lang.reflect.Method;
 
 public class _02_Enhancer {
     public String test() {
@@ -17,6 +16,7 @@ public class _02_Enhancer {
     public static void main(String[] args) {
         Enhancer enhancer = new Enhancer();
         enhancer.setSuperclass(_02_Enhancer.class);
+        // 一个Filter对应一个callback（用相同的数组下标）
         enhancer.setCallbackFilter(method -> {
             if (method.getReturnType() == String.class) {
                 return 0;
@@ -26,6 +26,31 @@ public class _02_Enhancer {
         });
         // 用FixedValue修正所有函数返回值
         enhancer.setCallbacks(new Callback[]{(FixedValue) () -> "hello cglib", NoOp.INSTANCE});
+        _02_Enhancer proxy = (_02_Enhancer) enhancer.create();
+
+        System.out.println(proxy.test());
+        System.out.println(proxy.test2());
+
+        // 上述方法需要自己对应filter和callback，可以用ClassBackHelper完成统一管理
+        main2();
+    }
+
+    private static void main2() {
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(_02_Enhancer.class);
+        // 和普通的filter不同的是,CallbackHelper不是返回CallBack[]的下标，而是CallBack
+        CallbackHelper callbackHelper = new CallbackHelper(_02_Enhancer.class, new Class[0]) {
+            @Override
+            protected Object getCallback(Method method) {
+                if (method.getReturnType() == String.class) {
+                    return (FixedValue) () -> "hello cglib";
+                }
+
+                return NoOp.INSTANCE;
+            }
+        };
+        enhancer.setCallbackFilter(callbackHelper);
+        enhancer.setCallbacks(callbackHelper.getCallbacks());
         _02_Enhancer proxy = (_02_Enhancer) enhancer.create();
 
         System.out.println(proxy.test());
