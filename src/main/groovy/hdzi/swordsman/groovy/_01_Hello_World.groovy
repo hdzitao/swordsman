0b5c3490039dceebcf5aaad2dfc9e2ba74f1f81b
hdzitao <hdzitao@qq.com>
2023-10-03 14:00:40 +0800

    hello world

--- src/main/groovy/hdzi/swordsman/groovy/_01_Hello_World.groovy
+++ src/main/groovy/hdzi/swordsman/groovy/_01_Hello_World.groovy
@@ -2,3 +2,9 @@ package hdzi.swordsman.groovy
 
 // groovy启动不需要main函数,当然groovy也可以像java一样启动
 println "Hello World"
+
+//class Hello {
+//    static void main(String[] args) {
+//        println "Hello World 2"
+//    }
+//}
